aad2be796db9757045f8209e4eb5c186cc5fc065
Sergei Gordeev <gordeser@fit.cvut.cz>
2024-08-22 13:23:41 +0200

    create post repository

---
+++ src/main/java/org/gordeser/arthub/Repository/PostRepository.java
@@ -0,0 +1,10 @@
+package org.gordeser.arthub.Repository;
+
+import org.gordeser.arthub.Entity.PostEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends JpaRepository<PostEntity, Long>, JpaSpecificationExecutor<PostEntity> {
+}
