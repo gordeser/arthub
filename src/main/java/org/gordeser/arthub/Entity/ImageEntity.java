ce0d752d3c50fa052c83e805c2f15553b42fd118
Sergei Gordeev <gordeser@fit.cvut.cz>
2024-08-22 13:10:15 +0200

    create image entity

---
+++ src/main/java/org/gordeser/arthub/Entity/ImageEntity.java
@@ -0,0 +1,32 @@
+package org.gordeser.arthub.Entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "image_entity")
+public class ImageEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "image_url")
+    private String imageUrl;
+
+    @OneToOne(mappedBy = "image")
+    // should be @JoinColumn(name = "post_id") ?
+    private PostEntity posts;
+}
