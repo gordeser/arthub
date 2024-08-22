7ac728a87fd31a438b028b3d6d9f9f909ff75364
Sergei Gordeev <gordeser@fit.cvut.cz>
2024-08-22 13:23:53 +0200

    create user repository

---
+++ src/main/java/org/gordeser/arthub/Repository/UserRepository.java
@@ -0,0 +1,9 @@
+package org.gordeser.arthub.Repository;
+
+import org.gordeser.arthub.Entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+}
