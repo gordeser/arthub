e4818c826af6e51ae281d4e2a60f22f9166b6a9f
Sergei Gordeev <gordeser@fit.cvut.cz>
2024-08-25 18:55:12 +0200

    delete JpaSpecificationExcecutor in repos

--- src/main/java/org/gordeser/arthub/Repository/FolderRepository.java
+++ src/main/java/org/gordeser/arthub/Repository/FolderRepository.java
@@ -1,10 +1,10 @@
 package org.gordeser.arthub.Repository;
 
 import org.gordeser.arthub.Entity.FolderEntity;
+import org.gordeser.arthub.Entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface FolderRepository extends JpaRepository<FolderEntity, Long>, JpaSpecificationExecutor<FolderEntity> {
+public interface FolderRepository extends JpaRepository<FolderEntity, Long> {
 }
--- src/main/java/org/gordeser/arthub/Repository/TagRepository.java
+++ src/main/java/org/gordeser/arthub/Repository/TagRepository.java
@@ -2,7 +2,6 @@ package org.gordeser.arthub.Repository;
 
 import org.gordeser.arthub.Entity.TagEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
